959dc4e541332abb5de5574e2de66f2e057bebbf
drpauloguimaraesjr <drpauloguimaraesjr@gmail.com>
2026-01-19 09:05:56 -0300

    Centralizar animacao loading com texto (marginLeft 30px)

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -44,7 +44,7 @@ export function AnimatedLogo({
   return (
     <div
       className="relative flex items-center justify-center"
-      style={{ width: size, height: size, overflow: "hidden" }}
+      style={{ width: size, height: size, overflow: "hidden", marginLeft: 30 }}
     >
       <Lottie
         lottieRef={lottieRef}
